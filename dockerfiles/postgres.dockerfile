20b17bb0c77672bcf7e3ced3b9749ec1ce531e0a
Peter Becich <peterbecich@gmail.com>
2017-12-14 19:38:20 -0800

    merge.sh

--- dockerfiles/postgres.dockerfile
+++
@@ -1,9 +0,0 @@
-FROM library/postgres:10
-
-MAINTAINER Peter Becich "peterbecich@gmail.com"
-
-ADD dockerfiles/init.sql /docker-entrypoint-initdb.d/
-
-# https://stackoverflow.com/questions/40717975/extend-docker-postgres-image-to-create-extra-database
-
-# https://hub.docker.com/_/postgres/
---
+++ travis/merge.sh
@@ -0,0 +1,28 @@
+#https://reformatcode.com/code/git/automatic-merge-branch-into-master-on-sucessful-build-in-travis
+
+echo "merge.sh"
+
+echo "disk free"
+df -h
+
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+    echo "merge into production; check out deep repo"
+    # https://stackoverflow.com/a/32580822/1007926
+    echo "git clone git@github.com:${TRAVIS_REPO_SLUG}.git"
+    git clone git@github.com:${TRAVIS_REPO_SLUG}.git $TRAVIS_REPO_SLUG
+    cd $TRAVIS_REPO_SLUG
+    echo "git checkout -qf $TRAVIS_COMMIT"
+    git checkout -qf $TRAVIS_COMMIT
+    echo "git checkout production"
+    git checkout production || exit
+    echo "git merge $TRAVIS_COMMIT"
+    git merge "$TRAVIS_COMMIT" || exit
+    echo "git push origin production"
+fi
+
+if [ "$TRAVIS_BRANCH" == "production" ]; then
+    echo "CI build of production branch successful"
+    docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+    echo "logged into Docker Hub"
+    docker push $TRAVIS_REPO_SLUG
+fi
